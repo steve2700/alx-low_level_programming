87b06130082f4e0a84ea8f1daa7a83d6069b85d1
steve2700 <stevnyaruwa@gmail.com>
2022-09-08 14:04:47 -0700

    size of c in the programming\n

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("size of a char: %ld byte(s)\n", sizeof(char));
+	printf("size of an int: %ld byte(s)\n", sizeof(int));
+	printf("size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %ld bytes(s)\n", sizeof(long long int));
+	printf("size of a float: %ld bytes(s)\n", sizeof(float));
+	return (0);
+}
