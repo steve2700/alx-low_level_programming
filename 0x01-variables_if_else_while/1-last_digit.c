21fad9ea55ede52224646620b194b103c395d566
steve2700 <stevnyaruwa@gmail.com>
2022-09-10 12:23:50 -0700

    rain network.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,6 +21,7 @@ int main(void)
 	if (lastn > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
+
 	} else if (lastn == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, lastn);
