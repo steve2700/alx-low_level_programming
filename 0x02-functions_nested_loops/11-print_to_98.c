822b623d932c7fd8860e578304c81e56acd94984
steve2700 <stevnyaruwa@gmail.com>
2022-09-14 04:46:05 -0700

    mf

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,16 +5,31 @@
  * in order seprated by a comma followed by a space.
  * @n: The number to begin counting at.
  */
+
 void print_to_98(int n)
 {
-	if (n >= 98)
+	int i;
+
+	if (n > 98)
 	{
-		for (n = n; n <= 97; n++)
-			printf("%d,", n);
-		printf("-98\n");
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	} else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
 	}
-	else
-		for (n = n; n > 98 ; n--)
-			printf("%d,", n);
-	printf("98\n");
+	printf("\n");
 }
