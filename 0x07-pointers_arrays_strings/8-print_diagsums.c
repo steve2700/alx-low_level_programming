03c57899dfa734e88c2ed93ffb434caff20ef2a5
steve2700 <stevnyaruwa@gmail.com>
2022-09-26 04:49:03 -0700

    dd

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,20 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_chessboard - board to print
- * @a: prints things
- * Return: returns crap
+ * print_diagsums - prints diagonal summaries
+ * @a: array to use
+ * @size: size of diagonal
  */
-void print_chessboard(char (*a)[8])
+void print_diagsums(int *a, int size)
 {
 	int i;
-	int j;
+	int diagO = 0;
+	int diagT = 0;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-		}
-		_putchar('\n');
+		diagO += a[(i * size) + i];
+		diagT += a[(size - 1) + ((size - 1) * i)];
 	}
+	printf("%d, %d\n", diagO, diagT);
 }
